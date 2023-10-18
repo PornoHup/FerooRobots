e254c3839a055cb5dfc3c41fc648d0882181dd3d
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-10-18 15:42:07 +0400

    update Mesaj.py

--- Mesajlar/Mesaj.py
+++ Mesajlar/Mesaj.py
@@ -1,1 +1,59 @@
+salam = (
+"Salam",
+"Salam necəsən",
+"Salam Ürəyimmm ❤️",
+"Salamdaa",
+"Uşş canım salam",
+"Salam Cənab 🫶",
+"Salam gözəl insan",
+)
+
+necesen = (
+"Saol",
+"Həkimsən ?",
+"Ə belədana 😂",
+"Sənə nə",
+"İyyim aşkım sen ?",
+"yaxşı olmağa çalışıram",
+"Mən başımı buraxe sən necəsən 😂",
+"Həyatdıda yaşadır",
+)
+
+sagol = (
+"Salam Sağol",
+"Hara gedsən",
+"Yatıram demə🥲",
+"Sağolunnn yenə gözləyəriyy🙈",
+"Uşş balam Sağol",
+"Sağol canım benim 🫶",
+"Sağol Kişi 🔥",
+"Sağol uğurlar",
+)
+
+getdim = (
+"Hara",
+"Getmə",
+"Get bidə gəlmə",
+)
+
+geldim = (
+"Xoş Gəldin ❤️",
+"Xoş gəldin nəfəs",
+"Xoş gəldin necəsən",
+)
+
+
+ban = (
+"Vəhşii",
+"Kimi ban edəcəksən",
+"Vəhşi Panteramm kimə ban atdın",
+"Havada ban kokusu var",
+)
+
+
+hardasan = (
+"Mən hər zaman burdayam",
+"Başqa groupda idim",
+"Sən hardasan",
+)
 
