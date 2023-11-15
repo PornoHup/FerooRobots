8f3e2b2a5ab610a8759bca46c28fc075445f849b
Theferid <122831032+Theferid@users.noreply.github.com>
2023-11-15 23:19:40 +0400

    Update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -19,7 +19,7 @@ class Config:
    OWNER_ID = int(os.environ.get("OWNER_ID","6297494131"))
    OWNER_NAME = os.environ.get("OWNER_NAME", "The_ferid") 
    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
-   MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://music:music@cluster0.sh6h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+   MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority")
    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001995989751"))
    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "ferooplaylist")
    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002091369970"))
