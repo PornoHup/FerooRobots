f0d8b29286a7660bfd560762ed5a099ea0850038
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2024-02-11 01:09:34 +0400

    update Broadcast.py

--- AylinRobot/Plugin/Broadcast.py
+++ AylinRobot/Plugin/Broadcast.py
@@ -126,3 +126,302 @@ async def broadcast_message(_, message):
         except Exception:
             pass
     await m.edit(f"✈️ **Broadcasted message in {sent} chats.**")
+
+
+
+ ##Broadcastall İkili Yayım Edər   
+    
+class Database: 
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id): # Veritabanına yeni kullanıcı ekler
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason="",
+            ),
+        )
+
+    async def add_user(self, id): # Veritabına yeni kullanıcı eklemek için ön def
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id): # Bir kullanıcının veritabında olup olmadığını kontrol eder.
+        user = await self.col.find_one({"id": int(id)})
+        return bool(user)
+
+    async def total_users_count(self): # Veritabanındaki toplam kullanıcıları sayar.
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self): # Veritabındaki tüm kullanıcıların listesini verir.
+        return self.col.find({})
+
+    async def delete_user(self, user_id): # Veritabından bir kullanıcıyı siler.
+        await self.col.delete_many({"id": int(user_id)})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason): # Veritabanınızdaki bir kullanıcıyı yasaklılar listesine ekler.
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
+        )
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+
+    async def remove_ban(self, id): # Veritabanınızdaki yasaklılar listesinde bulunan bir kullanıcın yasağını kaldırır.
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
+    async def get_ban_status(self, id): # Bir kullanıcın veritabanınızda yasaklılar listesinde olup olmadığını kontrol eder.
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("ban_status", default)
+
+    async def get_all_banned_users(self): # Veritabınızdaki yasaklı kullanıcılar listesini verir.
+        return self.col.find({"ban_status.is_banned": True})
+
+
+db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
+mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
+dcmdb = mongo_db_veritabani.handlers
+
+
+
+################## KULLANICI KONTROLLERİ #############
+async def handle_user_status(bot: Client, cmd: Message): # Kullanıcı kontrolü
+    chat_id = cmd.chat.id
+    if not await db.is_user_exist(chat_id):
+        if cmd.chat.type == "private":
+            await db.add_user(chat_id)
+            await app.send_message(Config.LOG_CHANNEL,LAN.BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id))
+        else:
+            await db.add_user(chat_id)
+            chat = bot.get_chat(chat_id)
+            if str(chat_id).startswith(f"{Config.LOG_CHANNEL}"):
+                new_chat_id = str(chat_id)[4:]
+            else:
+                new_chat_id = str(chat_id)[1:]
+            await app.send_message(Config.LOG_CHANNEL,LAN.GRUP_BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id, chat.title, cmd.chat.id, cmd.chat.id, cmd.message_id))
+
+    ban_status = await db.get_ban_status(chat_id) # Yasaklı Kullanıcı Kontrolü
+    if ban_status["is_banned"]:
+        if int((datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])).days) > int(ban_status["ban_duration"]):
+            await db.remove_ban(chat_id)
+        else:
+            if Config.SUPPORT:
+                msj = f"@{Config.SUPPORT}"
+            else:
+                msj = f"[{LAN.SAHIBIME}](tg://user?id={Config.OWNER_ID})"
+            if cmd.chat.type == "private":
+                await cmd.reply_text(LAN.PRIVATE_BAN.format(msj), quote=True)
+            else:
+                await cmd.reply_text(LAN.GROUP_BAN.format(msj),quote=True)
+                await app.leave_chat(cmd.chat.id)
+            return
+    await cmd.continue_propagation()
+
+async def broadcast_messages(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
+
+async def broadcast_messages_group(chat_id, message):
+    try:
+        kd = await message.copy(chat_id=chat_id)
+        try:
+            await kd.pin()
+        except:
+            pass
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages_group(chat_id, message)
+    except Exception as e:
+        return False, "Error"
+
+
+
+@app.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply)
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    blocked = 0
+    deleted = 0
+    failed =0
+    success = 0
+    async for user in users:
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+
+############### Broadcast araçları ###########
+broadcast_ids = {}
+
+
+async def send_msg(user_id, message): # Mesaj Gönderme
+    try:
+        if Config.BROADCAST_AS_COPY is False:
+            await message.forward(chat_id=user_id)
+        elif Config.BROADCAST_AS_COPY is True:
+            await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(int(e.x))
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id}: {LAN.NOT_ONLINE}\n"
+    except UserIsBlocked:
+        return 400, f"{user_id}: {LAN.BOT_BLOCKED}\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id}: {LAN.USER_ID_FALSE}\n"
+    except Exception:
+        return 500, f"{user_id}: {traceback.format_exc()}\n"
+
+async def main_broadcast_handler(m, db): # Ana Broadcast Mantığı
+    all_users = await db.get_all_users()
+    broadcast_msg = m.reply_to_message
+    while True:
+        broadcast_id = "".join(random.choice(string.ascii_letters) for i in range(3))
+        if not broadcast_ids.get(broadcast_id):
+            break
+    out = await m.reply_text(
+        text=LAN.BROADCAST_STARTED)
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total=total_users, current=done, failed=failed, success=success)
+    async with aiofiles.open("broadcast-logs-g4rip.txt", "w") as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id=int(user["id"]), message=broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user["id"])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(
+                    dict(current=done, failed=failed, success=success))
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    if failed == 0:
+        await m.reply_text(text=LAN.BROADCAST_STOPPED.format(completed_in, total_users, done, success, failed), quote=True,)
+    else:
+        await m.reply_document(document="broadcast-logs-g4rip.txt", caption=LAN.BROADCAST_STOPPED.format(completed_in, total_users, done, success, failed), quote=True,)
+    os.remove("broadcast-logs-g4rip.txt")
+
+################# SAHİP KOMUTLARI #############
+
+# Verileri listeleme komutu
+@app.on_message(command(["stats"]) & filters.user(Config.OWNER_ID))
+async def botstats(app: Client, message: Message):
+    g4rip = await app.send_message(message.chat.id, LAN.STATS_STARTED.format(message.from_user.mention))
+    all_users = await db.get_all_users()
+    groups = 0
+    pms = 0
+    async for user in all_users:
+        if str(user["id"]).startswith("-"):
+            groups += 1
+        else:
+            pms += 1
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    total_users = await db.total_users_count()
+    await g4rip.edit(text=LAN.STATS.format(Config.BOT_USERNAME, total_users, groups, pms, total, used, disk_usage, free, cpu_usage, ram_usage, __version__))
+
+
+
+# Botu ilk başlatan kullanıcıların kontrolünü sağlar.
+@app.on_message()
+async def G4RIP(app: Client, cmd: Message):
+    await handle_user_status(app, cmd)
+
+
+
+# Broadcast komutu
+@app.on_message(command("broadcastall") & filters.user(Config.OWNER_ID) & filters.reply)
+async def broadcast_handler_open(_, m: Message):
+    await main_broadcast_handler(m, db)
+
+
+
+############## BELİRLİ GEREKLİ DEF'LER ###########
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "K", 2: "M", 3: "G", 4: "T"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
